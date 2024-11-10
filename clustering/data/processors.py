6d64c721fa99bb494bb0ff2a802f29e100a80015
unknown <natalia.kaperczak@gmail.com>
2024-11-10 12:11:53 +0100

    Added clustering part and data, basic interface build

---
+++ clustering/data/processors.py
@@ -0,0 +1,13 @@
+class MSNBCDataProcessor:
+    def __init__(self):
+        pass
+
+    def load_data(self, file_path: str | None = None):
+        pass
+
+    def create_feature_vector(self, sequence) -> list[int]:
+        pass
+
+    def preprocess_sequence(self, sequences):
+        pass
+
---
+++ clustering/models/kmeans.py
@@ -0,0 +1,15 @@
+class kMeansClusterer:
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def get_cluster_centers(self):
+        pass
+
+    def get_labels(self, X):
+        pass
+
+    def get_silhouette(self, X):
+        pass
